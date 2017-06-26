cb028972cd08961ff95b32aee22bf916a0ffc7f0
Claudia Duarte <Clau@MacBook-Pro-de-Claudia.local>
2017-06-26 15:44:31 +0200

    movement

--- index.js
+++ index.js
@@ -1,14 +1,34 @@
 //Board
 
-var grid = [];
+var board = [];
 
-for (var x=0; x<24; x++) {
-  grid[x] = [];
-  for (var y=0; y<24;y++) {
-    grid[x][y] = ".";
+for (var x = 0; x < 24; x++) {
+  board[x] = [];
+  for (var y = 0; y < 24; y++) {
+    board[x][y] = ".";
   }
 }
 
 
-//Player
+//Pig
+$(document).on('keydown', function(e) {
+  var x = $("#player1").position().top;
+  if (event.keyCode === 38) {
+    if (x <= -246) {
+      return;
+    }
+    $("#player1").css({
+      top: x -= 10
+    });
+  } else if (event.keyCode === 40) {
+    if (x >= 300) {
+      return;
+    }
+    $("#player1").css({
+      top: x += 10
+    });
+  }
+});
+
+
 //Monsters
