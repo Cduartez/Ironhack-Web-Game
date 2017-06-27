52e783f46b18b9c3480f7430c0e65df701ec086e
Claudia Duarte <Clau@MacBook-Pro-de-Claudia.local>
2017-06-27 17:13:18 +0200

    NEW

--- index.js
+++ index.js
@@ -1,34 +1,18 @@
-//Board
+var board = new Board(4,3);
 
-var board = [];
-
-for (var x = 0; x < 24; x++) {
-  board[x] = [];
-  for (var y = 0; y < 24; y++) {
-    board[x][y] = ".";
-  }
+function paintBoard(board, body){
+  board.grid.forEach(function(rowArray, i){
+    var row = $('<div>').addClass('row');
+    for(var x = 0; x<rowArray.length; x++){
+      var column = $('<div>').addClass('column');
+      row.append(column);
+    }
+    body.append(row);
+  });
 }
 
+$(document).ready(function(){
+  var body = $('#board');
 
-//Pig
-$(document).on('keydown', function(e) {
-  var x = $("#player1").position().top;
-  if (event.keyCode === 38) {
-    if (x <= -246) {
-      return;
-    }
-    $("#player1").css({
-      top: x -= 10
-    });
-  } else if (event.keyCode === 40) {
-    if (x >= 300) {
-      return;
-    }
-    $("#player1").css({
-      top: x += 10
-    });
-  }
+  paintBoard(board, body);
 });
-
-
-//Monsters
