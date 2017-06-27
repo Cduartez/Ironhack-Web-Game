6769066fdbd7990384538febcf0262b57e7b8cd1
Claudia Duarte <Clau@MacBook-Pro-de-Claudia.local>
2017-06-27 20:14:56 +0200

    Done Characters Array

--- Board.js
+++ Board.js
@@ -1,9 +1,9 @@
 function Board(width, height){
-  this.width = width
-  this.height = height
-  this.grid = this._generateGrid()
+  this.width = width;
+  this.height = height;
+  this.grid = this._generateGrid();
 }
 
 Board.prototype._generateGrid = function(){
-  return Array.apply(null, Array(this.width)).map(e => Array(this.height))
-}
+  return Array.apply(null, Array(this.width)).map(e => Array(this.height));
+};
--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-var board = new Board(4,3);
+var board = new Board(4, 4);
 
-function paintBoard(board, body){
-  board.grid.forEach(function(rowArray, i){
+function paintBoard(board, body) {
+  board.grid.forEach(function(rowArray, i) {
     var row = $('<div>').addClass('row');
-    for(var x = 0; x<rowArray.length; x++){
+    for (var x = 0; x < rowArray.length; x++) {
       var column = $('<div>').addClass('column');
       row.append(column);
     }
@@ -11,8 +11,24 @@ function paintBoard(board, body){
   });
 }
 
-$(document).ready(function(){
+$(document).ready(function() {
   var body = $('#board');
-
   paintBoard(board, body);
+});
+var characters = new Characters(1, 8);
+
+function paintCharacters(board, body) {
+  characters.grid.forEach(function(rowArray, i) {
+    var row = $('<div>').addClass('rows');
+    for (var x = 0; x < rowArray.length; x++) {
+      var column = $('<div>').addClass('characters');
+      row.append(column);
+    }
+    body.append(row);
+  });
+}
+
+$(document).ready(function() {
+  var body = $('#board');
+  paintCharacters(board, body);
 });
