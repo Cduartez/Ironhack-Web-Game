4ddd5267d8d63a1ca6ded146758057d3938b6592
Claudia Duarte <Clau@MacBook-Pro-de-Claudia.local>
2017-06-28 16:11:12 +0200

    FIRST

--- characters.js
+++ characters.js
@@ -1,33 +1,11 @@
-function Characters (width, height){
+function Characters(width, height) {
   this.width = width;
   this.height = height;
   this.grid = this._generateGrid();
 }
 
-Characters.prototype._generateGrid = function(){
+Characters.prototype._generateGrid = function() {
   return Array.apply(null, Array(this.width)).map(e => Array(this.height));
 };
 
-
-
-
-
-
-// //var characters = ["Bob", "Peter", "Fred", "Richard", "Lois", "Pam", "Rachel", "x"];
-// var characters = new Characters(1, 8);
-//
-// function paintCharacters(board, body) {
-//   board.grid.forEach(function(rowArray, i) {
-//     var row = $('<div>').addClass('row');
-//     for (var x = 0; x < rowArray.length; x++) {
-//       var column = $('<div>').addClass('column');
-//       row.append(column);
-//     }
-//     body.append(row);
-//   });
-// }
-//
-// $(document).ready(function() {
-//   var characters = $('#board');
-//   paintCharacters(board, body);
-// });
+//DRAGG
--- index.js
+++ index.js
@@ -5,6 +5,9 @@ function paintBoard(board, body) {
     var row = $('<div>').addClass('row');
     for (var x = 0; x < rowArray.length; x++) {
       var column = $('<div>').addClass('column');
+      if(i===0 || i===3){
+        column.addClass('chair');
+      }
       row.append(column);
     }
     body.append(row);
@@ -14,14 +17,27 @@ function paintBoard(board, body) {
 $(document).ready(function() {
   var body = $('#board');
   paintBoard(board, body);
+
+  $('.chair').droppable( {
+    drop: handleDropEvent
+  } );
+
+
+function handleDropEvent( event, ui ) {
+  var draggable = ui.draggable;
+  alert( 'The panda with ID "' + draggable.attr('id') + '" was dropped onto me!' );
+}
 });
+
+
+
 var characters = new Characters(1, 8);
 
 function paintCharacters(board, body) {
   characters.grid.forEach(function(rowArray, i) {
-    var row = $('<div>').addClass('rows');
+    var row = $('<div>').addClass('row');
     for (var x = 0; x < rowArray.length; x++) {
-      var column = $('<div>').addClass('characters' + x);
+      var column = $('<div>').attr("id",'characters' + x);
       row.append(column);
     }
     body.append(row);
@@ -31,4 +47,12 @@ function paintCharacters(board, body) {
 $(document).ready(function() {
   var body = $('#game-board');
   paintCharacters(board, body);
+  $('#characters0').draggable();
+  $('#characters1').draggable();
+  $('#characters2').draggable();
+  $('#characters3').draggable();
+  $('#characters4').draggable();
+  $('#characters5').draggable();
+  $('#characters6').draggable();
+  $('#characters7').draggable();
 });
