2d4c9c3597e37cba8cf40b674e38eaeb9d838ed8
Claudia Duarte <Clau@MacBook-Pro-de-Claudia.local>
2017-06-30 13:46:16 +0200

    DONE IMAGES

--- javascript/Timer.js
+++ javascript/Timer.js
@@ -1,12 +1,12 @@
 function Timer() {
-  this.timer = 20;
-  this.timeOutId = 20;
+  this.timer = 40;
+  this.timeOutId = 40;
   this._checkTimer();
 }
 
 Timer.prototype._checkTimer = function() {
   $('#counter').text(this.timer);
-  if (this.timer >= 0 && this.timer <= 20) {
+  if (this.timer >= 0 && this.timer <= 40) {
     this.timer--;
     this.timeOutId = setTimeout(this._checkTimer.bind(this), 1000);
   } else {
